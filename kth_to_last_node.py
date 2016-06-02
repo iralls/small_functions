bfc3bdfa161ba9fca710e876facc1ead3a0c721d
iralls <ianralls19@gmail.com>
2016-06-01 22:45:14 -0400

    fixing mistake

--- kth_to_last_node.py
+++ kth_to_last_node.py
@@ -2,9 +2,16 @@ def kth_to_last_node(k, node, count=0):
     """
     Find the k-th to last node in a linked list
     """
-    if not node.next:
-        return kth_to_last_node(k, node.next, count)
-    else:
-        count += 1
-        if count == k:
-            return node
+    def go(k, node, acc):
+        if not node.next:
+            return (node, 1)
+        else:
+            (n, acc) = go(k, node.next, acc)
+
+            acc += 1
+            if acc == k:
+                return (node, acc)
+            else:
+                return (n, acc)
+
+    return go(k, node, 0)[0]
